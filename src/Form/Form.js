30d179e0e1a8f2cd0b664ef47e0d4ba349719276
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-10-31 09:20:20 +0500

    [Form section] -Changed and removed some lines of code according to PR

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,18 +1,22 @@
+import { useState, useEffect } from "react";
+
 import "./Form.css";
-import React, { useState } from "react";
 
 export default function Form() {
-  const [meme, setMeme] = React.useState({
+  const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-  const [allMemes, setAllMemes] = React.useState([]);
-  React.useEffect(() => {
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((data) => setAllMemes(data.data.memes));
   }, []);
+
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
     const url = allMemes[randomNumber].url;
@@ -40,24 +44,29 @@ export default function Form() {
           name="topText"
           value={meme.topText}
           onChange={handleChange}
+          placeholder="Top  text"
         />
+
         <input
           type="text"
           className="input-bottom"
           name="bottomText"
           value={meme.bottomText}
           onChange={handleChange}
+          placeholder="Bottom text"
         />
       </div>
+
       <div>
         <button onClick={getMemeImage} className="form-btn">
           Get a new meme image
         </button>
       </div>
+
       <div className="meme">
         <img src={meme.randomImage} className="meme-image"></img>
-        <h2 className="meme-text top">{meme.topText}</h2>
-        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+        <h2 className="meme-text top-text">{meme.topText}</h2>
+        <h2 className="meme-text bottom-text">{meme.bottomText}</h2>
       </div>
     </div>
   );
